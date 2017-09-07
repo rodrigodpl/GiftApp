5c0cdcff2644e40be11c06750d81bf7256781230
rodrigodpl <rodrigodpl07@gmail.com>
2017-09-07 17:51:27 +0200

    profile activity progress

--- app/src/main/java/appteam/app/MyProfileActivity.java
+++ app/src/main/java/appteam/app/MyProfileActivity.java
@@ -5,9 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    TextView profile_name;
+    boolean isEditable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,26 @@ public class MyProfileActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        profile_name = (TextView) findViewById(R.id.prflname_editable);
+        isEditable = false;
+    }
+
+    public void SetProfilenameEditable(View view){
+
+
+        if(!isEditable) {
+            profile_name.setFocusable(true);
+            profile_name.setFocusableInTouchMode(true);
+            profile_name.setHint(profile_name.getText());
+            profile_name.setText("");
+            isEditable = true;
+        }
+        else{
+            profile_name.setFocusable(false);
+            isEditable = false;
+        }
+
     }
 
 }
